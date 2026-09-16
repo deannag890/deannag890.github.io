dbdf47ec1adddeae6ca733538fda15d49c748bb7
deannag890 <deannagonzalez890@gmail.com>
2026-09-16 15:35:02 -0500

    Completed lab 3

--- javascript/external.js
+++ javascript/external.js
@@ -1,2 +1,3 @@
 var user_name = window.prompt("Please enter your name", "Type your name here");
 document.write(user_name);
+document.getElementByID("Welcome").innerHTML = "Hello, Welcome" + user_name + "!";
