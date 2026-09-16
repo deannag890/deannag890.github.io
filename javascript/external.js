546565647bc236835aad8cd8a4f23ab50fbeada6
deannag890 <deannagonzalez890@gmail.com>
2026-09-16 14:55:51 -0500

    Saved username.

--- javascript/external.js
+++ javascript/external.js
@@ -1,5 +1,2 @@
-arr = ["Mael", "Eric", "Randall", "James"];
-for (var i=0; i arr.length; i++)
-{
-  document.writeIn(arr[i], "<br>");
-}
+var user_name = window.prompt("Please enter your name", "Type your name here");
+document.write(user_name);
